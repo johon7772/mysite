970f601280f89c6cf11bd9bdf0514298c3451ffc
johonalimov <johon7772@gmail.com>
2021-02-26 11:03:57 +0500

    urok 15

--- news/views.py
+++ news/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
 
 
 def index(request):
-    # print(dir(request))
-    return HttpResponse('Hello world')
+    news = News.objects.all()
+    context = {'news': news,
+               'title': 'Список новостей'}
+    return render(request, template_name='news/index.html', context=context)
 
-
-def test(request):
-    return HttpResponse('<h1>Тестовая страница</h1>')
